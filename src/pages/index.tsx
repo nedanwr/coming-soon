83590c2433098423e02126a5f6827fad8cc961f5
Naveed <46922100+nedanwr@users.noreply.github.com>
2021-07-23 14:40:53 +0500

    chore: Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Home extends Component<any, any> {
+    render(): JSX.Element {
+        return (
+            <h1>Hello World</h1>
+        );
+    }
+}
+
+export default Home;
